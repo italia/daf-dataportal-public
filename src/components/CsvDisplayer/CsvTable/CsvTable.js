8295d54a5ee8ddbc984767d61ad5796374d3e20e
Francesco <programmingzone@gmail.com>
2017-10-08 02:28:05 +0200

    Refactored package Map component

--- src/components/CsvDisplayer/CsvTable/CsvTable.js
+++ src/components/CsvDisplayer/CsvTable/CsvTable.js
@@ -5,23 +5,30 @@ import TableFoot from './TableFoot.js'
 import * as csv from "csvtojson"
 import * as request from "request";
 
+/**
+ * 
+ * @author: Davide Pastore
+ * @author: Cosimo,Francesco Pichierri
+ * 
+ * Visualize CSV content into a Table
+ */
 export default class CsvTable extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            headers: [],
-            rows: []
+            headers: [], //Headers
+            rows: [] //Rows
         };
     }
     
     componentDidMount() {
         this.setState({
-            caption: this.props.caption ? this.props.caption: '',
-            noHeader: this.props.noHeader ? this.props.noHeader: false,
-            showFoot: this.props.showFoot ? this.props.showFoot: false,
-            rows: this.props.rows ? this.props.rows: [],
-            headers: this.props.headers ? this.props.headers: []
+            caption: this.props.caption ? this.props.caption: '', //Table's caption
+            noHeader: this.props.noHeader ? this.props.noHeader: false, //true if you want skip the header
+            showFoot: this.props.showFoot ? this.props.showFoot: false, //true if you want show footer
+            rows: this.props.rows ? this.props.rows: [], //Rows containing data
+            headers: this.props.headers ? this.props.headers: [] //Headers
         });
     }
 
--- src/components/CsvDisplayer/map/CsvMap.js
+++ src/components/CsvDisplayer/map/CsvMap.js
@@ -8,20 +8,31 @@ import { Map, InfoControl, TileLayer, Marker } from 'react-leaflet-wrapper';
  */
 
 export default class CsvMap extends Component {
-
+    /**
+     * 
+     * @param {*} props 
+     */
     constructor(props){
         super(props);
         this.latLngs=[];
         this.state = {
             markers: []
         };
+        this.nativeLeafMarkers = [];
+        this.selectedMarker = null;
     }
     componentDidMount(){
         this.state.markers=this.props.rows ? this.props.rows : [] ;
     }
 
+    /**
+     * Given a row  and latitude field name, longitude field Name , returns a Leaflet
+     * marker
+     * @param {*} curMarker 
+     * @param {*} latFieldName 
+     * @param {*} longFieldName 
+     */
     getLatLng(curMarker,latFieldName,longFieldName){
-        console.log("currentMarker ",curMarker );
         return {
                     lat: curMarker[latFieldName], 
                     lng: curMarker[longFieldName]
@@ -37,6 +48,12 @@ export default class CsvMap extends Component {
         }
         
     }
+    /**
+     * Given a row  and a data column field Name returns 
+     * description of marker. This description will be rendered in a popup
+     * @param {*} curMarkerCsv 
+     * @param {*} dataColumnName 
+     */
     getDataColumn(curMarkerCsv, dataColumnName){
         var curLabel= '';
         if(curMarkerCsv && curMarkerCsv[dataColumnName]){
@@ -44,6 +61,10 @@ export default class CsvMap extends Component {
         }
         return curLabel;
     }
+
+    /**
+     * Render component
+     */
     render(){
         const rows = this.props.rows;
         const {center}=this.props;
@@ -51,8 +72,10 @@ export default class CsvMap extends Component {
         const {latFieldName} = this.props;
         const {dataColumnName} = this.props;
         const {zoom} = this.props;
-        console.log("MARKERS ",this.props.rows);
+        const {selected} = this.props;
+        //console.log("MARKERS ",this.props.rows);
         const {autoCenter} = this.props;
+        //console.log("selectedMarker ",this.selectedMarker);
         return (
         <div>
         <Map
@@ -72,7 +95,7 @@ export default class CsvMap extends Component {
                                         <Marker key={index}
                                         latlng={this.getLatLng(markerCsv,latFieldName,longFieldName)}
                                         popupContent={feature => <span>{this.getDataColumn(markerCsv,dataColumnName)}</span>}
-                                        />   
+                                        />  
                                 )
         }
         
