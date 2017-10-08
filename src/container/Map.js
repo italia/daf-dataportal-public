47281046330f9c5570ee0d544ab28905f439e96f
Francesco <programmingzone@gmail.com>
2017-10-08 12:11:38 +0200

    Added two maps on a single page

--- src/container/Map.js
+++ src/container/Map.js
@@ -21,16 +21,27 @@ class Map extends Component {
                 <div className="u-background-white u-layout-r-withGutter u-posRelative u-zindex-30">
                     <div className="u-layout-wide u-layoutCenter u-layout-withGutter u-padding-r-top u-padding-bottom-xxl">
                         <div className="Grid Grid--withGutter">
-                            <div className="Grid-cell u-md-size12of12 u-lg-size12of12 u-padding-right-xl">
+                        <div className="Grid-cell u-md-size12of12 u-lg-size12of12 u-padding-right-xl u-margin-r-top">  
+                                <h2>Visualizzazione componente mappa senza dettaglio</h2>
+                                <span>Clicca su un marker per vedere la descrizione apparire nel popup</span>
+                        </div>
+                        <div className="Grid-cell u-md-size12of12 u-lg-size12of12 u-padding-right-xl u-margin-r-top">
                             <CsvDisplayer src={src}
                                     latFieldName='Lat' longFieldName='Lon' noheader={false} dataColumnName="Luogo"
                                     type="map"  zoom="18" center={[40.36135,18.18825]}></CsvDisplayer>   
-
-                                 <CsvDisplayer src={src}
-                                    latFieldName='Lat' longFieldName='Lon' noheader={false} dataColumnName="Luogo"
-                                    type="map"  zoom="18" center={[40.36135,18.18825]} showDetail={true}></CsvDisplayer>   
                             </div>
+
+                        
+                        <div className="Grid-cell u-md-size12of12 u-lg-size12of12 u-padding-right-xl u-margin-r-top">  
+                                <h2>Visualizzazione componente mappa con dettaglio</h2>
+                                <span>Clicca su un marker per vederne il dettaglio</span>
                         </div>
+                        <div className="Grid-cell u-md-size12of12 u-lg-size12of12 u-padding-right-xl u-margin-r-top">
+                        <CsvDisplayer src={src}
+                                    latFieldName='Lat' longFieldName='Lon' noheader={false} dataColumnName="Luogo"
+                                    type="map"  zoom="18" center={[40.36135,18.18825]} showDetail={true}></CsvDisplayer>       
+                        </div>    
+                    </div>
                     </div>
                 </div>
             </div>
