5a7bb70b9189d9c0123649ca375207d9f049360c
Marco Matta <marco.matta@videomed.biz>
2017-10-08 14:46:42 +0200

    wip

--- src/container/Map/MapsList.js
+++ src/container/Map/MapsList.js
@@ -2,16 +2,13 @@ import React from 'react'
 
 const MapsList = ({maps, onMapClick,}) =>
     <div style={{marginRight: "120px"}}>
-        <h4>Lista</h4>
-        <ol>
-            <li style={{display: "flex", flexDirection: "column", flexWrap: "wrap"}}>
-                {maps.map((current) => {
-                    const {name, data} = current
-                    return <div>
-                        <a key={name} style={{margin: '10px'}} onClick={() => onMapClick(data, name)}>{name}</a>
-                    </div>;
-                })}
-            </li>
+        <ol className="Linklist Linklist--padded u-layout-prose u-text-r-xs Treeview Treeview--default js-Treeview">
+            {maps.map((current) => {
+                const {name, data} = current
+                return <li>
+                    <a key={name} style={{margin: '10px'}} onClick={() => onMapClick(data, name)}>{name}</a>
+                </li>
+            })}
         </ol>
     </div>
 
